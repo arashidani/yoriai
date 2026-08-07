ce19d0f32b7acb784ebf886b47277b58a5771c89
MaayaYokoo <maayaaaa.o2.ll@gmail.com>
2026-08-07 09:48:50 +0900

    [update]登録フォームのパスワード条件追加

--- lib/schemas/register.ts
+++ lib/schemas/register.ts
@@ -6,7 +6,12 @@ export const registerFormSchema = z.object({
     .string()
     .min(1, 'メールアドレスを入力してください')
     .email('メールアドレスの形式が正しくありません'),
-  password: z.string().min(8, 'パスワードは8文字以上で入力してください'),
+  password: z
+    .string()
+    .min(8, 'パスワードは8文字以上で入力してください')
+    .regex(/^[a-zA-Z0-9]+$/, { message: 'パスワードは半角英数字で入力してください' })
+    .regex(/[a-zA-Z]/, { message: 'パスワードは半角英字を含めてください' })
+    .regex(/[0-9]/, { message: 'パスワードは半角数字を含めてください' }),
 })
 
 export type RegisterFormInput = z.infer<typeof registerFormSchema>
