fb9b752da2992374d62d370759d3e3355feb28c5
yuya.arashidani <100942704+arashidani@users.noreply.github.com>
2026-08-28 17:33:58 +0900

    fix: 質問のタイトルを48文字に制限

--- components/design-system/question-form-modal.tsx
+++ components/design-system/question-form-modal.tsx
@@ -85,6 +85,7 @@ export function QuestionFormModal({
           <FormField
             label="質問のタイトル"
             error={errors.title?.message}
+            maxLength={48}
             inputProps={{
               id: 'title',
               placeholder: '例：キングオブタイムの有給申請について',
